21092e60555f6d5c0b16ffa26fa9e4dcf1477dfb
orlandouchiha425 <valadez425@gmail.com>
2022-05-13 23:56:19 -0400

    removed route from appjs and imported images to homepage

--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -1,26 +1,21 @@
 import { useState } from 'react';
 import {Routes, Route} from 'react-router-dom'
+import AuthPage from '../AuthPage/AuthPage';
 import HomePage from '../Homepage';
 
 export default function App() {
-  const [user,setUser] = useState()
+ 
 
 
   return (
     <div className = "App">
      
      <Routes>
-      {
-        user?
-        <>
-        
+
+    
         <Route path='/' element={<HomePage />} />
         
-        </>
-        :
-        <Route path='/' element={<HomePage/>}/>
-
-      }
+   
 
 
      </Routes>
---
+++ src/pages/AuthPage/AuthPage.js
@@ -0,0 +1,16 @@
+
+ 
+import { useState } from "react";
+
+export default function AuthPage({ setUser }){
+    
+    return(
+        <main>
+            <div>
+            <h1>this is the AuthPage</h1>
+            </div>
+
+        </main>
+    )
+
+}
