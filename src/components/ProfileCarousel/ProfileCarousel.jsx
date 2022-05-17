f0c31c43051fe270c485563408e5eba10e34eb23
orlandouchiha425 <valadez425@gmail.com>
2022-05-17 16:00:20 -0400

    re pushing roys updates

--- src/components/ProfileCarousel/ProfileCarousel.jsx
+++ src/components/ProfileCarousel/ProfileCarousel.jsx
@@ -1,20 +1,30 @@
-import anime1 from "./ImagesCarousel/anime1.jpeg"
-import anime2 from "./ImagesCarousel/anime2.jpeg"
-import anime3 from "./ImagesCarousel/anime3.jpeg"
+// import anime1 from "./ImagesCarousel/anime1.jpeg"
+// import anime2 from "./ImagesCarousel/anime2.jpeg"
+// import anime3 from "./ImagesCarousel/anime3.jpeg"
 import ImageUploads from "../ImageUploads/ImageUploads"
+import { useState } from "react"
 
 export default function ProfileCarousel(){
+const [image, setImage] = useState('')
+
 
     return(
+      
         <div className="ImageContainer">
-<div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+       
+   
   <div class="carousel-indicators">
+  
+<ImageUploads image={image} setImage={setImage}/>
+
     <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
     <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
     <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
   </div>
   <div class="carousel-inner">
+    
     <div class="carousel-item active">
+
       <img src={image} class="d-block w-100" alt="First Image"/>
     
     </div>
@@ -34,7 +44,8 @@ export default function ProfileCarousel(){
     <span class="visually-hidden">Next</span>
   </button>
 </div>
-</div>
+
+
     )
 }
 
