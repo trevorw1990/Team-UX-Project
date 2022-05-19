9b4593046ecc0a934925bd2e94c44dd39ff40beb
Mindy Miller <mmillerks@gmail.com>
2022-05-18 20:10:02 -0500

    about pg css initial update

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -4,71 +4,98 @@ export default function About() {
 
     return (
         <div>
-            <h1> Meet the Team </h1>
-            <div className="team">
-                <div>
-                    <h2> Mindy Miller </h2> 
-                    <img src = './images/About/mindy.png' alt="Mindy"/>
-                    <p> Project Manager </p>
-                    <p> Front-End CSS </p>
-                    <a href="https://github.com/mmillerks"> Github : mmillerks</a> 
+
+            <div id="aboutTitle">
+                <h1> Meet the Team </h1>
+            </div>
+            
+            <div className="team"> 
+                
+                <div id="columnOneTeam">
+                    <div className='teamMember'>
+                        <img src = './images/About/mindy.png' alt="Mindy"/>
+                        <h2> Mindy Miller </h2> 
+                        <p> Project Manager </p>
+                        <p> Front-End CSS </p>
+                        <a href="https://github.com/mmillerks"> Github : mmillerks</a>
+                    </div>
+                    
+                    <div className='teamMember'>
+                        <img src = './images/About/moses.jpeg' alt="Moses"/>
+                        <h2> Moses Burkhart </h2>
+                        <p> GIT Czar </p>
+                        <a href="https://github.com/mosesb1"> Github : mosesb1 </a>
+                    </div>
+                    
+                    <div className='teamMember'>
+                        {/* <img src/> */}
+                        <h2> Orlando Valadez </h2>
+                        <p> Front-End </p>
+                        <a href="https://github.com/Orlandouchiha425"> Github: Orlandouchiha425</a> 
+                    </div>
                 </div>
-                <div>
-                    <h2> Moses Burkhart </h2>
-                    <img src = './images/About/moses.jpeg' alt="Moses"/>
-                    <p> GIT Czar </p>
-                    <a href="https://github.com/mosesb1"> Github : mosesb1 </a>
+
+                <div id="columnOneTeam">
+                    <div className='teamMember'>
+                        <img src='./images/About/ashley.JPG' alt='Ashley'/>
+                        <h2> Ashley Webb </h2>
+                        <p> Front-End </p>
+                        <a href="https://github.com/ashleywebb"> Github : ashleywebb </a>
+                    </div>
+                
+                    <div className='teamMember'>
+                        <img src='./images/About/roy-pic.jpg' alt='Roy'/>
+                        <h2> Roy Daniel </h2>
+                        <p> Full-Stack </p>
+                        <a href="https://github.com/fxcircus"> Github : fxcircus </a>
+                    </div>
+                
+                    <div className='teamMember'>
+                        <img src='./images/About/damon.jpg' alt='Damon'/>
+                        <h2> Damon Fung</h2>
+                        <p> Back-End </p>
+                        <a href="https://github.com/dfung003"> Github : dfung003</a>
+                    </div>
                 </div>
-                <div>
-                    <h2> Orlando Valadez </h2>
-                    {/* <img src/> */}
-                    <p> Front-End </p>
-                    <a href="https://github.com/Orlandouchiha425"> Github: Orlandouchiha425</a> 
+
+                <div id="columnOneTeam">
+                    
+                    <div className='teamMember'>
+                        <img src='./images/About/Trevor.png' alt="Trevor"/>
+                        <h2> Trevor Whitehurst </h2>
+                        <p> Full Stack </p>
+                        <a href="https://github.com/trevorw1990"> Github : trevorw1990 </a>
+                    </div>
+
+                    <div className='teamMember'>
+                        <img src='./images/About/jonathan.png' alt="Jonathan"/>
+                        <h2> Jonathan Suarez </h2>
+                        <p> Back-End </p>
+                        <a href="https://github.com/jonsuarez92"> Github : jonsuarez92 </a>
+                    </div>
+              
+                    <div className='teamMember'>
+                        <img src="./images/About/kajsa.jpeg" alt="Kajsa"/>
+                        <h2> Kajsa Brown </h2>
+                        <p> UX Designer </p> 
+                    </div>
                 </div>
-                <div>
-                    <h2> Ashley Webb </h2>
-                    <img src='./images/About/ashley.JPG' alt='Ashley'/>
-                    <p> Front-End </p>
-                    <a href="https://github.com/ashleywebb"> Github : ashleywebb </a>
-                </div>
-                <div>
-                    <h2> Roy Daniel </h2>
-                    <img src='./images/About/roy-pic.jpg' alt='Roy'/>
-                    <p> Full-Stack </p>
-                    <a href="https://github.com/fxcircus"> Github : fxcircus </a>
-                </div> 
-                <div>
-                    <h2> Damon Fung</h2>
-                    <img src='./images/About/damon.jpg' alt='Damon'/>
-                    <p> Back-End </p>
-                    <a href="https://github.com/dfung003"> Github : dfung003</a>
-                </div>
-                <div>
-                    <h2> Trevor Whitehurst </h2>
-                    <img src='./images/About/Trevor.png' alt="Trevor"/>
-                    <p> Full Stack </p>
-                    <a href="https://github.com/trevorw1990"> Github : trevorw1990 </a>
-                </div>
-                <div>
-                    <h2> Jonathan Suarez </h2>
-                    <img src='./images/About/jonathan.png' alt="Jonathan"/>
-                    <p> Back-End </p>
-                    <a href="https://github.com/jonsuarez92"> Github : jonsuarez92 </a>
-                </div>
-                <div>
-                    <h2> Kajsa Brown </h2>
-                    <img src="./images/About/kajsa.jpeg" alt="Kajsa"/>
-                    <p> UX Designer </p> 
-                </div>
-                <div>
-                    <h2> Austin O'Brien </h2> 
-                    <img src="./images/About/austin.jpeg" alt="Austin"/>
-                    <p> UX Designer </p>
-                </div>
-                <div>
-                    <h2> Sean O'Brien </h2>
-                    <img src="./images/About/sean.jpeg" alt="Sean"/>
-                    <p> Instructional Assistant </p>
+
+                <div id="columnOneTeam">
+                    
+                    <div className='teamMember'>
+                        <img src="./images/About/austin.jpeg" alt="Austin"/>
+                        <h2> Austin O'Brien </h2> 
+                        <p> UX Designer </p>
+                    </div>
+                    
+                    <div className='teamMember'>
+                        <img src="./images/About/sean.jpeg" alt="Sean"/>
+                        <h2> Sean O'Brien </h2>
+                        <p> General Assembly</p>
+                        <p> Instructional Assistant </p>
+                    </div>
+
                 </div>
                 <div>
 
