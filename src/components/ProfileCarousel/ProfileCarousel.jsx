1a1a5e06c098f4d956a9cbf493307aead16dfd3a
Roy Daniel <roydanielr@gmail.com>
2022-05-20 07:58:24 -0400

    carousel now updates dynamically each time the user uploads a new image

--- src/components/ProfileCarousel/ProfileCarousel.jsx
+++ src/components/ProfileCarousel/ProfileCarousel.jsx
@@ -1,17 +1,9 @@
-// import anime1 from "./ImagesCarousel/anime1.jpeg"
-// import anime2 from "./ImagesCarousel/anime2.jpeg"
-// import anime3 from "./ImagesCarousel/anime3.jpeg"
 import ImageUploads from "../ImageUploads/ImageUploads"
 import { useState, useEffect } from "react"
 
 export default function ProfileCarousel(){
 const [image, setImage] = useState('')
-const [carousel, setCarousel] = useState([
-  "",
-  "",
-  "",
-])
-
+const [carousel, setCarousel] = useState([''])
 const [counter, setCounter ] = useState(0)
 const [formData, setFormData] = useState({
   profileImageUrl: '',
@@ -33,53 +25,55 @@ useEffect(() => {
 }, [image])
   
   return(
-  <div className="ImageContainer">
-    
-    <div id="carouselExampleIndicators" className="carousel slide" data-bs-ride="carousel">
-
-    <div className="carousel-indicators">
-      <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
-      <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
-      <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
-    </div>
-
-    <div className="carousel-inner">
-      <div className="carousel-item active">
-        <img src={carousel[0] ? carousel[0] : ""} className="d-block w-100" alt="First Image"/>
-      </div>
-
-      {
-        carousel[1] ?
-        <div className="carousel-item">
-          <img src={carousel[1] ? carousel[1] : ""} className="d-block w-100 " alt="Second Image"/>
-        </div>
-        :
-        doNothing()
-      }
+    carousel[0] ?
+    <div className="ImageContainer">
       
-      {
-        carousel[2] ?
-        <div className="carousel-item">
-          <img src={carousel[2] ? carousel[2] : ""} className="d-block w-100 " alt="Second Image"/>
+      <div id="carouselExampleIndicators" className="carousel slide" data-bs-ride="carousel">
+
+        <div className="carousel-indicators">
+          {
+            carousel.map((pic, idx) => {
+                return (
+                  idx === 0 ?
+                  <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
+                  :
+                  <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to={idx} aria-label={`Slide ${idx}`}></button>
+              )
+            })
+          }
         </div>
-        :
-        doNothing()
-      }
+
+        <div className="carousel-inner">
+          {
+            carousel.map((pic, idx) => {
+                return (
+                  idx === 0 ?
+                  <div className="carousel-item active">
+                    <img src={carousel[0]} className="d-block w-100" alt="First Image"/>
+                  </div>
+                  :
+                  <div className="carousel-item">
+                    <img src={carousel[idx]} className="d-block w-100 " alt="Other Image"/>
+                  </div>
+              )
+            })
+          }
+        </div>
+
+        <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+          <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span className="visually-hidden">Previous</span>
+        </button>
+
+        <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+          <span className="carousel-control-next-icon" aria-hidden="true"></span>
+          <span className="visually-hidden">Next</span>
+        </button>
+
+      </div>
+      <ImageUploads image={image} setImage={setImage}/>
     </div>
-
-    <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
-      <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-      <span className="visually-hidden">Previous</span>
-    </button>
-
-    <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
-      <span className="carousel-control-next-icon" aria-hidden="true"></span>
-      <span className="visually-hidden">Next</span>
-    </button>
-
-    </div>
+    :
     <ImageUploads image={image} setImage={setImage}/>
-  </div>
   )
 }
-
