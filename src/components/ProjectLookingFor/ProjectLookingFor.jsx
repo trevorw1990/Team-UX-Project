fa0752b5a72cecf7a888a363e5a7ebff0b84960b
Moses Burkhart <moses.burkhart1@gmail.com>
2022-05-21 14:16:14 -0400

    added role display and invite and im interested button to looking for display

--- src/components/ProjectLookingFor/ProjectLookingFor.jsx
+++ src/components/ProjectLookingFor/ProjectLookingFor.jsx
@@ -1,6 +1,6 @@
 import ProjectLookingForItem from '../ProjectLookingForItem/ProjectLookingForItem';
 import './ProjectLookingFor.css';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 export default function ProjectLookingFor({project, user}){
     const [roleDisplay, setRoleDisplay] = useState([]);
@@ -8,7 +8,7 @@ export default function ProjectLookingFor({project, user}){
     const getRoleDisplay = () => {
         setRoleDisplay(project.lookingForItems.map((item, idx) => {
             return (
-                <ProjectLookingForItem key={idx} item={item}/>
+                <ProjectLookingForItem key={idx} item={item} project={project} user={user}/>
             )
         }))
     }
@@ -16,5 +16,21 @@ export default function ProjectLookingFor({project, user}){
     useEffect(() => {
         getRoleDisplay();
     },[])
-    return <h1>ProjectLookingFor</h1>
+
+    const loaded = () => {
+        return (
+            <div>
+                <hr/>
+                <h3>Looking For</h3>
+                {roleDisplay}
+                <hr/>
+            </div>
+        )
+    }
+
+    const loading = () => {
+        return
+    }
+
+    return roleDisplay.length ? loaded() : loading()
 }
--- src/components/ProjectLookingForItem/ProjectLookingForItem.jsx
+++ src/components/ProjectLookingForItem/ProjectLookingForItem.jsx
@@ -1,5 +1,18 @@
 import './ProjectLookingForItem.css';
 
-export default function ProjectLookingForItem(){
-    return <h1>Item</h1>
+export default function ProjectLookingForItem({item, user, project}){
+    const isCollaborator = () => {
+        for(let i = 0; i < project.collaborators.length; i++){
+            if(user._id === project.collaborators[i].userId){
+                return true
+            }
+        }
+        return false
+    }
+    return (
+        <div>
+            <h4>{item}</h4>
+            <button>{user._id === project.organizer || isCollaborator() ? 'Invite' : "I'm interested"}</button>
+        </div>
+    )
 }
