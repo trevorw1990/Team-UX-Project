a54f4269bf5469c2fe82db8e0eb79821bd560e76
Mindy Miller <mmillerks@gmail.com>
2022-05-21 22:07:32 -0500

    inbox css

--- src/components/Inbox/InboxNavBar.jsx
+++ src/components/Inbox/InboxNavBar.jsx
@@ -4,11 +4,15 @@ export default function InboxNavBar({ user, setUser, pageToShow,  setPageToShow}
 
     return (
         <div>
+            <div className='inbox-header'>
+                <h1>Inbox</h1>
+            </div>
+
             <div className='inbox-navbar-categories'>
-                <button onClick={(e) => {setPageToShow('InboxUnread')}}>Unread</button>
-                <button onClick={(e) => {setPageToShow('InboxAllMail')}}>All Mail</button>
-                <button onClick={(e) => {setPageToShow('InboxSent')}}>Sent</button>
-                <button onClick={(e) => {setPageToShow('InboxDrafts')}}>Drafts</button>
+                <button onClick={(e) => {setPageToShow('InboxUnread')}}>Unread</button><br/>
+                <button onClick={(e) => {setPageToShow('InboxAllMail')}}>All Mail</button><br/>
+                <button onClick={(e) => {setPageToShow('InboxSent')}}>Sent</button><br/>
+                <button onClick={(e) => {setPageToShow('InboxDrafts')}}>Drafts</button><br/>
                 <button onClick={(e) => {setPageToShow('InboxTrash')}}>Trash</button>
             </div>
         </div>
