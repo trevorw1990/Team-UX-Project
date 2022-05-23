412090da3ca19d9ae89f18084bae9bc24ebfbd85
Moses Burkhart <moses.burkhart1@gmail.com>
2022-05-23 18:03:16 -0400

    edited profile page user area to conditionally render based on url

--- src/pages/ProfilePage/ProfilePage.js
+++ src/pages/ProfilePage/ProfilePage.js
@@ -1,20 +1,36 @@
 import ProfileCarousel from "../../components/ProfileCarousel/ProfileCarousel"
 import { useParams } from "react-router-dom"
 import { useState, useEffect } from 'react'
+import { getUser } from "../../utilities/api/users/users-api";
 import ProfilePageUserArea  from '../../components/ProfilePageUserArea/ProfilePageUserArea'
 import ProfilePageProjects from '../../components/ProfilePageProjects/ProfilePageProjects'
 
 export default function ProfilePage({ user, setUser }){
+    const [profileUser, setProfileUser] = useState(null)
     const params = useParams() 
+    const id = params.id
+
+    const findUser = async () => {
+        try {
+            const foundUser = await getUser(id);
+            setProfileUser(foundUser);
+        } catch (err) {
+            console.error(err);
+        }
+    }
+
+    useEffect(() => {
+        findUser()
+    },[])
 
 
     const loaded = () => {
         return(
             <main className="profile-pg-container">
 
-                <ProfilePageUserArea user={user} setUser={setUser}/>
+                <ProfilePageUserArea profileUser={profileUser} user={user} setUser={setUser}/>
                 
-                <ProfilePageProjects user={user} setUser={setUser}/>
+                <ProfilePageProjects profileUser={profileUser} user={user} setUser={setUser}/>
                 
                 <div className="gallery-header">
                     <h2>My Gallery</h2>
@@ -39,5 +55,5 @@ export default function ProfilePage({ user, setUser }){
     }
 
 
-    return user && user._id ? loaded() : loading()
+    return profileUser && profileUser._id ? loaded() : loading()
 }
