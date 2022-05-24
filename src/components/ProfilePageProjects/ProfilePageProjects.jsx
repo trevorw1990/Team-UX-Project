4ef423179bf6736795e5dd9e9509223b74a8bb70
Moses Burkhart <moses.burkhart1@gmail.com>
2022-05-23 20:17:53 -0400

    removed console log

--- src/components/ProfilePageProjects/ProfilePageProjects.jsx
+++ src/components/ProfilePageProjects/ProfilePageProjects.jsx
@@ -9,7 +9,6 @@ export default function ProfilePageProjects({ user, profileUser }) {
     const getProjects = async () => {
         try {
             const foundProjects = await getUserProjects(profileUser._id);
-            console.log(foundProjects)
             setProjects(foundProjects);
         } catch (err) {
             console.error(err);
